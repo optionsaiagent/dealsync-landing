9e6e1648f2508f787faa313f613e6c7fbb5e6565
Jay M <madkow21@Jays-Mac-mini.local>
2026-04-12 15:17:06 -1000

    Fix index.js - remove duplicate imports

--- src/index.js
+++ src/index.js
@@ -7,22 +7,4 @@ root.render(
   <React.StrictMode>
     <LandingPage />
   </React.StrictMode>
-);import React from 'react';
-import ReactDOM from 'react-dom/client';
-import LandingPage from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <LandingPage />
-  </React.StrictMode>
-);import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
 );
