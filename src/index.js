cf9e6bf091311b955291113f3bdecae20ee905a8
Jay M <madkow21@Jays-Mac-mini.local>
2026-04-12 15:10:29 -1000

    Fix duplicate React import

--- src/index.js
+++ src/index.js
@@ -8,6 +8,15 @@ root.render(
     <LandingPage />
   </React.StrictMode>
 );import React from 'react';
+import ReactDOM from 'react-dom/client';
+import LandingPage from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <LandingPage />
+  </React.StrictMode>
+);import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
